4870c7d3e28735e1c848a5229dc5d8cfa0e9a8db
People403 <135725113+people403@users.noreply.github.com>
2024-11-06 15:43:48 +0700

    Update checkramdisk.sh

--- checkramdisk.sh
+++ checkramdisk.sh
@@ -4,7 +4,7 @@
 #!/bin/bash
 
 clear
-echo -n "Check \003[32mRamdisk\003[0m/\003[31mNonramdisk \003[32mLoading..."
+echo -e -n "Check \003[32m Ramdisk \003[0m / \003[31m Nonramdisk \003[32 mLoading..."
 
 for i in $(seq 1 10); do
     echo -n ""
@@ -14,16 +14,16 @@ done
 clear
 sleep 4
 if grep -q "rootfs" /proc/mounts; then
-    echo "\033[33mYour Phone \003[32mUse Ramdisk."
-    echo "If there are bugs/errors, report them to @magiskpeople gmail"
+    echo -e "\033[33m Your Phone \003[32m Use Ramdisk."
+    echo -e "\003[31m If there are bugs/errors, \003[32m report them to @magiskpeople gmail"
 else
-    echo "\033[32mYour Phone \033[31mNot Use Ramdisk."
-    echo "\003[31mIf there are bugs/errors, \003[32mreport them to @magiskpeople gmail"
+    echo -e "\033[32m Your Phone \033[31mNot Use Ramdisk."
+    echo -e "\003[31m If there are bugs/errors, \003[32m report them to @magiskpeople gmail"
 fi
 sleep 4
 clear
-echo "\033[32mDon't Forget Read \003[31mReadme \033[33mRamdisk/Nonramdisk"
+echo -e "\033[32m Don't Forget Read \003[31m Readme \033[33m Ramdisk/Nonramdisk"
 sleep 4
 clear
 sleep 4
-echo "\003[32mDone!"
+echo -e "\003[32mDone!"
