6520ef8fd3367f7f69988d8b17126690890bbce4
People403 <135725113+people403@users.noreply.github.com>
2024-11-06 16:33:38 +0700

    Update checkramdisk.sh

--- checkramdisk.sh
+++ checkramdisk.sh
@@ -9,8 +9,6 @@ echo -n "Checking Your Use Ramdisk/Nonramdisk Loading..."
 for i in $(seq 1 10); do
     echo -n "."
     sleep 0.5
-    # Menempatkan kursor di awal baris
-    echo -ne "\rChecking Your Use Ramdisk/Nonramdisk Loading..."
 done
 
 
@@ -18,16 +16,15 @@ clear
 sleep 4
 
 if grep -q "rootfs" /proc/mounts; then
-    echo -e "\033[31mYour Phone Use Ramdisk. \n"
-    echo \n
-    echo -e "\033[31mIf There Are Bugs/Errors, Report Them To @magiskpeople Gmail \n"
+    echo "\033[31mYour Phone Use Ramdisk. \n"
+    echo "\033[31mIf There Are Bugs/Errors, Report Them To @magiskpeople Gmail \n"
 else
-    echo -e "\033[31mYour Phone Not Use Ramdisk. \n"
-    echo -e "\033[31mIf There Are Bugs/Errors, Report Them To @magiskpeople Gmail\033[0m"
+    echo "\033[31mYour Phone Not Use Ramdisk. \n"
+    echo "\033[31mIf There Are Bugs/Errors, Report Them To @magiskpeople Gmail\033[0m"
 fi
 
 sleep 10
 clear
-echo -e "Don't Forget Read Readme Ramdisk/Nonramdisk \n"
+echo "Don't Forget Read Readme Ramdisk/Nonramdisk \n"
 sleep 4
-echo -e "Done!"
+echo "Done!"
